4bc37206149ab6d8b965ae19deeda6469c2557eb
yashvi-param <yashvirwn123@gmail.com>
2026-08-13 06:36:00 +0530

    Delete javascript directory

--- javascript/03_control_flow/01_if-else.js
+++
@@ -1,7 +0,0 @@
-let marks = 30;
-
-if(marks >= 35){
-    console.log("you have passed this examination");
-} else {
-    console.log("you have failed in this exam");
-}
--- javascript/extra/script.js
+++
@@ -1,5 +0,0 @@
-let products = ["vegetables", "fruits", "shoes", "clothes"];
-
-for (let name of products) {
-  console.log(name);
-}
