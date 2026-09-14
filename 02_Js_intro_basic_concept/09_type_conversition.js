41b3a6d40770ff4a5c68efd4a651582ac965f28c
yashvi-param <yashvirwn123@gmail.com>
2026-09-14 12:43:49 +0530

    Add JavaScript class work

---
+++ 02_Js_intro_basic_concept/09_type_conversition.js
@@ -0,0 +1,28 @@
+console.log(5 - "5");
+console.log(5 + "5");
+
+//
+
+let num1 = "10";
+let num2 = 10;
+
+console.log(num1 + num2);
+
+console.log(Number(num1) + num2);
+
+// parseInt;
+
+num1 = "100x";
+
+console.log(parseInt(num1) + num2);
+
+// parseFloat
+
+console.log(parseFloat(num1) + num2);
+
+let value = 24;
+
+console.log("type of value", typeof value);
+
+value = String(100);
+console.log("type of value", typeof value);
