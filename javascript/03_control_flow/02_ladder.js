b80ef84cd8c09c74a5795eb8309579fd078a0e0b
yashvi-param <yashvirwn123@gmail.com>
2026-09-14 12:49:48 +0530

    ldder.js complete

---
+++ javascript/03_control_flow/02_ladder.js
@@ -0,0 +1,14 @@
+// let marks = 60;
+let marks = 86;
+
+if (marks >= 85 && marks <= 100) {
+  console.log("you have achieved A grade");
+} else if (marks >= 70 && marks < 85) {
+  console.log("you have achieved b grade");
+} else if (marks >= 55 && marks < 70) {
+  console.log("you have achieved c grade");
+} else if (marks >= 35 && marks < 55) {
+  console.log("you have achieved D grade");
+} else {
+  console.log("you have failed this examination");
+}
