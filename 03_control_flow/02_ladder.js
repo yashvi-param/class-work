33402d8efd75809f70eee9f6edd8830f5045289e
yashvi-param <yashvirwn123@gmail.com>
2026-09-14 12:25:18 +0530

    Add control flow ladder example

---
+++ 03_control_flow/02_ladder.js
@@ -0,0 +1,13 @@
+let marks = 20;
+
+if(marks >= 85 && marks <= 100){
+    console.log("you have achieved A Grade");
+}else if(marks >= 70 && marks < 85){
+    console.log("you have achieved B Grade");
+}else if(marks >= 55 && marks < 70){
+    console.log("you have achieved B Grade");
+}else if(marks >= 35 && marks < 55){
+    console.log("you have achieved B Grade");
+}else {
+    console.log("you have failed in this examination")
+}
