8fee446ccf1eb1b0448184ff2e3e0997d70a16e0
yashvi-param <yashvirwn123@gmail.com>
2026-09-18 12:40:42 +0530

    arrow function is complete

---
+++ javascript/05_function/03_arrow_function.js
@@ -0,0 +1,13 @@
+function normal(){
+    console.log ("this is normal function");
+}
+
+normal();
+
+let arrowFunction = () => {
+    console.log("this is arrow function");
+};
+
+let arrowFunction2 = () => console.log("this is arrow function");
+arrowFunction();
+arrowFunction2()
