37c50449e7e1b2d30264aa5d785ae933e08b5f8d
yashvi-param <yashvirwn123@gmail.com>
2026-09-18 12:15:38 +0530

    function complete 1 file

---
+++ javascript/05_function/01_function.js
@@ -0,0 +1,15 @@
+// function declaration
+
+function play(){
+    console.log ("i am playing");
+}
+
+play();
+
+// function expression
+
+const activity = function (){
+    console.log("i currently doing my running practice");
+}
+
+activity()
