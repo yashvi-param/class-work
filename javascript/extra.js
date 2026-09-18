a9de74e793f653142718eaa107f43a351175a020
yashvi-param <yashvirwn123@gmail.com>
2026-09-18 12:21:38 +0530

    parameters and argument are complete

---
+++ javascript/extra.js
@@ -0,0 +1,15 @@
+function play(game) {
+  console.log("i am playing", game);
+}
+
+play("gta-5");
+play("forza horizon-5");
+
+function activity(activity="exercise") {
+  console.log("i am doing", activity);
+}
+
+
+activity()
+
+activity("running")
